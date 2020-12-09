a24c55da1314c49dec6627f050f175c859ff36ff
Harmin <freelancingonupwork@gmail.com>
2020-12-09 17:45:17 +0530

    Survey Update
    Survey Update

--- js/main.js
+++ js/main.js
@@ -123,4 +123,10 @@
     4: 'The username portion of the email address is invalid (the portion before the @: )',
     5: 'This email address looks fake or invalid. Please enter a real email address'
   };
+
+  $("#survey").steps({
+    headerTag: "h3",
+    bodyTag: "section",
+    transitionEffect: "slideLeft",
+});
 })(jQuery);
