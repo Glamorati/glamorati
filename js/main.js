1e6db5b09db604a82e773f8724b4f4af263a89af
Glamorati <74396512+Glamorati@users.noreply.github.com>
2021-01-18 15:21:18 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -131,16 +131,5 @@
     autoFocus: true,
     enableFinishButton: false,
 });
-$.fn.steps.setStep = function (step)
-{
-
-    var options = getOptions(this),
-        state = getState(this);
-
-    return _goToStep(this, options, state, index); //Index Instead step
-
-};
-
-survey.steps("setStep", 1);
   
 })(jQuery);
